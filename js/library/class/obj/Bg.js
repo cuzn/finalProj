4b41f6aca0461e7513f4c336fb0ecb33a37f5f45
cuzn <370050465@qq.com>
2014-03-07 19:19:31 +0800

    补交

---
+++ js/library/class/obj/Bg.js
@@ -0,0 +1,25 @@
+define(function(require, exports, module) {
+  	console.log('load screenTool')
+    var G = require('global');
+    var Obj = require('class/Obj');
+
+    var Bg = function(img) {
+    	var me = this;
+
+    	me.__proto__ = new Obj();
+
+    	me.img = img;
+
+    	me.draw = function() {
+    		G.draw(function() {
+    			G.ctx.drawImage(me.img , 0 ,0 , 640 , 960)
+    		});
+    	}
+
+    	me.setImg = function(img) {
+    		me.img = img;
+    	}
+    }
+
+    module.exports = Bg;
+})
---
+++ js/library/template.js
@@ -0,0 +1,11 @@
+/**
+ * 文件描述
+ */
+define(function(require, exports, module) {
+  	console.log('load screenTool')
+    var G = require('global');
+
+
+
+    module.exports = 
+})
